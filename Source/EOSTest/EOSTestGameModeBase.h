6aee3b423e3e42c3680207c190bc03517d7cfc59
Mojatto_WS <daichiandroid1997@gmail.com>
2020-03-24 20:43:17 +0900

    プロジェクトを初期化．

---
+++ Source/EOSTest/EOSTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EOSTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EOSTEST_API AEOSTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
